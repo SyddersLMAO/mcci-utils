b23d1aff7428337f8bc3c0c457a406ece5ea8e2e
SyddersLMAO <sydneyewens06@gmail.com>
2026-06-15 03:58:36 +0100

    base project boilerplate

---
+++ src/main/java/com/sydders/mcciutils/MCCIUtils.java
@@ -0,0 +1,15 @@
+package com.sydders.mcciutils;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MCCIUtils implements ModInitializer {
+	public static final String MOD_ID = "mcciutils";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/sydders/mcciutils/MCCIUtilsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.sydders.mcciutils;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MCCIUtilsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
